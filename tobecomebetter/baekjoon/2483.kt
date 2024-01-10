59a6f554f4237c651ae166403874b7a9b7628805
김경호 <kkh9331@naver.com>
2024-01-10 22:46:03 +0900

    [baekjoon] 약수, 소수 4번

---
+++ tobecomebetter/baekjoon/2483.kt
@@ -0,0 +1,22 @@
+package com.kguard.tobecomebetter.baekjoon
+//gcd 최대공약수를 구하는 함수를 재귀함수로 작성함
+// List.reduce를 사용해서 재귀함수를 사용하는 방식으로 풀음
+// acc는 이전 결과들, i는 이번 차례 변수 acc+i 는 acc+= i 를 뜻함
+// https://kotlinworld.com/32
+fun main() {
+    val a = readln().toInt()
+    val n = MutableList(a) { readln().toInt() }
+    val between = MutableList(a - 1) { n[it + 1] - n[it] }
+    val min  = between.reduce{acc, i -> gcd(acc,i)}
+    var count = 0
+    for(i in between){
+        count += i/min-1
+    }
+    println(count)
+}
+
+fun gcd(a: Int, b: Int): Int =
+    if (b == 0)
+        a
+    else
+        gcd(b, a % b)
