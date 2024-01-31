7d080682ab0104550381c2e8813feb080643c14d
김경호 <kkh9331@naver.com>
2024-01-31 23:11:13 +0900

    [baekjoon] 스택,큐,덱 10번

---
+++ tobecomebetter/baekjoon/2346.kt
@@ -0,0 +1,52 @@
+package com.kguard.tobecomebetter.baekjoon
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import kotlin.math.absoluteValue
+
+fun main() {
+    readln()
+    val deque = ArrayList<Pair<Int, Int>>()
+    readln().split(" ").forEachIndexed { index, s ->
+        deque.add(Pair(index + 1, s.toInt()))
+    }
+    while (true) {
+        val count = deque.removeFirst()
+        print("${count.first} ")
+        if (deque.isEmpty())
+            break
+        if (count.second > 0)
+            for (i in 1 until count.second) // 위에서 removeFirst를 하기 때문에
+                deque.add(deque.removeFirst())
+        else
+            for (i in 1..count.second.absoluteValue) //절대값
+                deque.add(0,deque.removeLast())
+    }
+}
+//fun main() {
+//    val br = BufferedReader(InputStreamReader(System.`in`))
+//    val bw = BufferedWriter(OutputStreamWriter(System.out))
+//    val str = StringBuilder()
+//    br.readLine()
+//    val deque = ArrayList<Pair<Int, Int>>()
+//    br.readLine().split(" ").forEachIndexed { index, s ->
+//        deque.add(Pair(index + 1, s.toInt()))
+//    }
+//    while (true) {
+//        val count = deque.removeFirst()
+//        str.append("${count.first} ")
+//        if (deque.isEmpty())
+//            break
+//        if (count.second > 0)
+//            for (i in 1 until count.second) // 위에서 removeFirst를 하기 때문에
+//                deque.add(deque.removeFirst())
+//        else
+//            for (i in 1..count.second.absoluteValue) //절대값
+//                deque.add(0,deque.removeLast())
+//    }
+//    bw.write(str.toString())
+//    bw.flush()
+//    bw.close()
+//}
