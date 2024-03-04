de225b787f0188f3f4f6c8e82f8e133f724e29ee
김경호 <kkh9331@naver.com>
2024-03-04 16:15:40 +0900

    [baekjoon] 백트래킹 4번

--- tobecomebetter/baekjoon/*15652.kt
+++ tobecomebetter/baekjoon/*15652.kt
@@ -1,42 +1,35 @@
-package com.kguard.tobecomebetter.baekjoon
-
 import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 
 //실버 3
-// boolean 부분 제거 하여 모든 값 출력
-
+// num을 추가하여서 처음 값보다 큰 것만 출력하도록 하여서 중복 제거
 // 1 -> 2, 3, 4
 // 2 -> 1, 3, 4
 // 3 -> 1, 2, 4,
 // 4 -> 1, 2, 3 으로 진행
-private val br = BufferedReader(InputStreamReader(System.`in`))
-private val bw = BufferedWriter(OutputStreamWriter(System.out))
 private var arr = mutableListOf<Int>()
 private var visited = mutableListOf<Boolean>()
 private var n = 0
 private var m = 0
 fun main() {
-    val a = br.readLine().split(" ").map { it.toInt() }
+    val a = readln().split(" ").map { it.toInt() }
     n = a[0]
     m = a[1]
     arr = MutableList(m) { 0 }
     visited = MutableList(n) { false }
-    dfs(0)
-    bw.flush()
-    bw.close()
+    dfs(0, 0)
 }
 
-private fun dfs(len: Int) {
+private fun dfs(num: Int, len: Int) {
     if (len == m) {
-        arr.forEach { bw.write("$it ") }
-        bw.write("\n")
+        arr.forEach { print("$it ") }
+        println()
         return
     }
-    for (i in 0 until n) {
+    for (i in num until n) {
         arr[len] = i + 1
-        dfs(len + 1)
+        dfs(i, len + 1)
     }
 }
