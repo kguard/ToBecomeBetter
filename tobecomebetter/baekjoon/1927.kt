9d421cba3332a86b7d392de8a8d75967b3a9b8f2
김경호 <kkh9331@naver.com>
2024-05-10 19:09:37 +0900

    [baekjoon] 우선순위 큐 2번 1927

--- tobecomebetter/baekjoon/1927.kt
+++ tobecomebetter/baekjoon/1927.kt
@@ -4,9 +4,10 @@ import java.util.PriorityQueue
 
 // 실버 2
 // 우선순위 큐, 자료구조
+// 최소 힙을 구하는 문제
 fun main(){
     val n = readln().toInt()
-    val queue = PriorityQueue<Int>()
+    val queue = PriorityQueue<Int>() // () 안에 아무것도 없으면 오름차순
     repeat(n){
         val a = readln().toInt()
         if(a == 0){
