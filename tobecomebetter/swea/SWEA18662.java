0d43d40710fe64abf8fab1379a86c294e1866dc6
김경호 <kkh9331@naver.com>
2024-05-17 20:01:15 +0900

    [swea] 18662. 등차수열 만들기

---
+++ tobecomebetter/swea/SWEA18662.java
@@ -0,0 +1,16 @@
+package com.kguard.tobecomebetter.swea;
+
+import java.util.Scanner;
+
+public class SWEA18662 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        for (int i = 1; i <= tc; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            System.out.println("#" + i + " "+Math.abs(((c - b) - (b - a)) / 2.0));
+        }
+    }
+}
