1aea0d3d236258e4786bfa1d91b50ef9594ea431
김경호 <kkh9331@naver.com>
2024-06-23 20:40:42 +0900

    [baekjoon] 그래프와 순회 14번 16928. 뱀과 사다리 게임

--- tobecomebetter/baekjoon/16928.kt
+++ tobecomebetter/baekjoon/16928.kt
@@ -2,6 +2,9 @@ package com.kguard.tobecomebetter.baekjoon
 
 // 골드 5 뱀과 사다리 게임
 // 그래프 이론, 그래프 탐색, 너비 우선 탐색
+// bfs를 사용하여 주사위가 6까지 이기 모두 움직이는 최소 거리를 구하는 식
+// 움직였을때 사다리나 뱀을 만나면 이동
+// 1부터 100까지 가는 최소거리
 fun main() {
     val (n, m) = readln().split(" ").map { it.toInt() }
     val ladderSnake = mutableMapOf<Int, Int>()
@@ -13,7 +16,7 @@ fun main() {
         ladderSnake[a] = b
     }
     fun bfs() {
-        queue.add(1)
+        queue.add(1) // 1부터 시작
         visited[1] = true
         while (queue.isNotEmpty()) {
             val poll = queue.removeFirst()
