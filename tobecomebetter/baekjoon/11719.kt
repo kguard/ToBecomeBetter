3c1ad13a64ee0811c6edc22f5f9b7c04411f3f86
김경호 <kkh9331@naver.com>
2025-01-17 05:12:29 +0900

    [baekjoon] 11719. 그대로 출력하기 2 (구현, 문자열) [readlnOrNull() 사용]

---
+++ tobecomebetter/baekjoon/11719.kt
@@ -0,0 +1,10 @@
+package com.kguard.tobecomebetter.baekjoon
+
+// 브론즈 3 그대로 출력하기 2
+// 구현, 문자열
+fun main() {
+    while (true) {
+        val n = readlnOrNull() ?: return
+        println(n)
+    }
+}
