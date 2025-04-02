a8efb091fdbebeb0282a5c7dd2a1ae28f4dccfbc
김경호 <kkh9331@naver.com>
2025-04-02 21:08:44 +0900

    [baekjoon] 2447. 별 찍기 - 10 (분할 정복, 재귀)

--- tobecomebetter/baekjoon/*2447.kt
+++ tobecomebetter/baekjoon/*2447.kt
@@ -5,12 +5,12 @@ import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 
-//골드 3
+// 골드 3 별 찍기 - 10
+// 분할 정복, 재귀
 // 문제를 좌표로 푸는 것이 맞음
 private val br = BufferedReader(InputStreamReader(System.`in`))
 private val bw = BufferedWriter(OutputStreamWriter(System.out))
 fun main() {
-
     val n = br.readLine().toInt()
     for (y in 0 until n) {
         for (x in 0 until n) {
@@ -22,11 +22,11 @@ fun main() {
     bw.close()
 }
 
-fun star(x: Int, y: Int, s: Int) {
+private fun star(x: Int, y: Int, s: Int) { // 재귀 방식으로 풀기
     if ((x / s) % 3 == 1 && (y / s) % 3 == 1)
         bw.write(" ")
     else if (s / 3 == 0)
         bw.write("*")
     else
-        star(x, y, s / 3)
+        star(x, y, s / 3) // 제귀 하는 부분
 }
