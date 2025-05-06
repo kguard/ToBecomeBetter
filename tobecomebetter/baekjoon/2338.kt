15d0cbcdc941df6a4a2945b1be37bc6fa9a3e3c9
김경호 <kkh9331@naver.com>
2025-05-07 02:37:34 +0900

    [baekjoon] 2338. 긴자리 계산 (수학, 사칙연산, 임의 정밀도 / 큰 수 연산)

---
+++ tobecomebetter/baekjoon/2338.kt
@@ -0,0 +1,11 @@
+package com.kguard.tobecomebetter.baekjoon
+
+// 브론즈 5 긴자리 계산
+// 수학, 사칙연산, 임의 정밀도 / 큰 수 연산
+fun main() {
+    val a = readln().toBigInteger()
+    val b = readln().toBigInteger()
+    println(a + b)
+    println(a - b)
+    println(a * b)
+}
