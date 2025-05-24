6ea8927e23f241930a030e3ad4472aebf2284466
김경호 <kkh9331@naver.com>
2025-05-24 12:09:03 +0900

    [SWEA] 1926. 간단한 369게임 D2

---
+++ tobecomebetter/swea/SWEA1926.java
@@ -0,0 +1,18 @@
+package com.kguard.tobecomebetter.swea;
+
+import java.util.*;
+
+//1926. 간단한 369게임 D2
+public class SWEA1926 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            String is = Integer.toString(i);
+            if (is.contains("3") || is.contains("6") || is.contains("9"))
+                is = is.replaceAll("[1245780]", "").replaceAll("[369]", "-");
+            System.out.print(is + " ");
+        }
+    }
+}
