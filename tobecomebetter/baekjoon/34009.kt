42d521c2b504c5684bd6b27e1211f1d864f1502c
김경호 <kkh9331@naver.com>
2025-06-03 01:39:21 +0900

    [baekjoon] 34009. Bob부 멍충이 (애드 혹)

---
+++ tobecomebetter/baekjoon/34009.kt
@@ -0,0 +1,12 @@
+package com.kguard.tobecomebetter.baekjoon
+
+// 브론즈 2 Bob부 멍충이
+// 애드 혹
+fun main() {
+    val n = readln().toInt()
+    val list = readln()
+    if(n % 2 == 1)
+        println("Bob")
+    else
+        println("Alice")
+}
