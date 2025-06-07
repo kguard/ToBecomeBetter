24963ded01ba5a4a66ebf77abb6468cadacab6a0
김경호 <kkh9331@naver.com>
2025-06-08 07:01:36 +0900

    [baekjoon] 1264. 모음의 개수 (구현, 문자열)

---
+++ tobecomebetter/baekjoon/14626.kt
@@ -0,0 +1,29 @@
+package com.kguard.tobecomebetter.baekjoon
+
+// 브론즈 1 ISBN
+// 수학, 구현, 브루트포스 알고리즘, 사칙연산
+// 마지막에 i를 for문을 돌리는 부분이 어이가 없음 -> 단순 노가다
+fun main() {
+    val list = readln()
+    var sum = 0
+    var weight = 0
+    for (i in list.indices) {
+        if (list[i] != '*') {
+            sum += if (i % 2 == 0)
+                list[i].toString().toInt() * 1
+            else
+                list[i].toString().toInt() * 3
+        } else {
+            weight = if (i % 2 == 0)
+                1
+            else
+                3
+        }
+    }
+    sum %= 10
+    for (i in 0..9)
+        if ((sum + i * weight % 10) % 10 == 0) {
+            print(i)
+            break
+        }
+}
