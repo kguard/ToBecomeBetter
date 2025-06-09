5db64c48a460429b0d863916f3752dce13ad352a
김경호 <kkh9331@naver.com>
2025-06-09 20:48:21 +0900

    [baekjoon] 14645. 와이버스 부릉부릉 (구현)

---
+++ tobecomebetter/baekjoon/14645.kt
@@ -0,0 +1,9 @@
+package com.kguard.tobecomebetter.baekjoon
+
+// 브론즈 5 와이버스 부릉부릉
+// 구현
+fun main() {
+    var (n, k) = readln().split(" ").map { it.toInt() }
+    repeat(n) { readln() }
+    print("비와이")
+}
