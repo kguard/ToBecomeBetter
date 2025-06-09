9d16da5ec35b8fe535eb274c0902cd7ff2cbac58
김경호 <kkh9331@naver.com>
2025-06-10 01:47:43 +0900

    [baekjoon] 24883. 자동완성 (구현)

---
+++ tobecomebetter/baekjoon/24883.kt
@@ -0,0 +1,11 @@
+package com.kguard.tobecomebetter.baekjoon
+
+// 브론즈 5 자동완성
+// 구현
+fun main(){
+    val t = readln()
+    if (t == "n" || t == "N")
+        println("Naver D2")
+    else
+        println("Naver Whale")
+}
