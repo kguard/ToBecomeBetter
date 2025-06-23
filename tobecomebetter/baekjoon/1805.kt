c33d199182fb55872cfca551390eef2d7929dda3
김경호 <kkh9331@naver.com>
2025-06-24 02:20:58 +0900

    [baekjoon] 1805. Moo (구현)

---
+++ tobecomebetter/baekjoon/1805.kt
@@ -0,0 +1,12 @@
+package com.kguard.tobecomebetter.baekjoon
+
+// 브론즈 5 Moo
+// 구현
+// kotlin이 아니라 GolfScript로 구현 (문제에서 GolfScript만 사용 가능)
+
+//'(___)
+//(o o)____/
+//@@      \
+//\ ____,/
+////   //
+//^^   ^^'
