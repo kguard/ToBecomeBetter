eedde083094d746298cb23ac1f176aebb1ad07ec
김경호 <kkh9331@naver.com>
2025-06-27 04:25:01 +0900

    [baekjoon] 16394. 홍익대학교 (수학, 사칙연산) {Java로 풀어야 해서 Java 파일 추가}

---
+++ tobecomebetter/baekjoon/16394.kt
@@ -0,0 +1,8 @@
+package com.kguard.tobecomebetter.baekjoon
+
+// 브론즈 5 홍익대학교
+// 수학, 사칙연산
+// java로 풀어야 해서 Java 파일 생성
+fun main() {
+    println(readln().toInt() - 1946)
+}
---
+++ tobecomebetter/baekjoon/baekjoon16394.java
@@ -0,0 +1,14 @@
+package com.kguard.tobecomebetter.baekjoon;
+
+import java.util.Scanner;
+
+// 브론즈 5 홍익대학교
+// 수학, 사칙연산
+// java로 풀어야 됨
+public class baekjoon16394 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n-1946);
+    }
+}
